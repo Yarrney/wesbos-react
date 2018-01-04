ce62ba4d05b51ca9fea584e11c7f6c176851852b
Yarrney <alex.arney@gmail.com>
2018-01-04 16:38:16 +1100

    finished component structure

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+import Fish from './Fish';
+
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
-					<Header/>
-					<Fish/>
+					<Header tagline="Fresh Seafood market" />
+					<Fish />
 				</div>
 				<Order />
 				<Inventory />
---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Invetory extends React.Component {
+	render() {
+		return (
+			<p>Invetory</p>
+		)
+	}
+}
+
+export default Invetory;
